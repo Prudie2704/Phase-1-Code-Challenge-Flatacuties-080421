ebd8d3c2a18afe1770608fe249d8b7347ee122f6
prudence <prudence.jerono@student.moringaschool.com>
2023-03-27 00:44:17 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-// Your code here
+let charcterInfo = document.getElementById("characterInfo")
+
+characterInfo.append(character-bar)
+
+//fetch function
+fetch ("http://localhost:3000/user")
+  .then(res => res.json())
+  .then(json => {
+    json.map(data =>{
+        console.log(data)
+        characterInfo.append(character-bar(data))
+    })
+  })
+  
+  //create character-bar
+  function character-bar(img.name.votes) { 
+    let character-bar = document.createElement(characterInfo)
+    characterInfo.innerHTML =
+    <div class="characterInfo">
+      <div id="detailed-info">
+        <p id="name">Character's Name</p>
+        <img
+          id="image"
+          src="assets/dummy.gif"
+          alt="Character's Name"
+        />
+        <h4>Total Votes: <span id="vote-count">Character's Votes</span></h4>
+        <form id="votes-form">
+          <input type="text" placeholder="Enter Votes" id="votes" name="votes" />
+          <input type="submit" value="Add Votes" />
+        </form>
+        <button id="reset-btn">Reset Votes</button>
+      </div>
+  }
